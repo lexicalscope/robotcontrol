e0c0440531e937a91779fa64671fe1fde3552853
lexicalscope <lexicalscope@gmail.com>
2017-12-26 22:56:04 +0000

    send back distance readings

--- robot-server.py
+++ robot-server.py
@@ -14,11 +14,34 @@ args = parser.parse_args()
 
 
 async def robot(websocket, path):
+    consumer_task = asyncio.ensure_future(robot_recv(websocket))
+    producer_task = asyncio.ensure_future(robot_send(websocket))
+    done, pending = await asyncio.wait(
+        [consumer_task, producer_task],
+        return_when=asyncio.FIRST_COMPLETED,
+    )
+
+    for task in pending:
+        task.cancel()
+
+
+async def robot_send(websocket):
+    while True:
+        await asyncio.sleep(0.1)
+        distance = rr.get_distance()
+        await websocket.send(json.dumps({'data': {'distance': distance}}))
+
+
+async def robot_recv(websocket):
     async for message in websocket:
         command = json.loads(message)
 
         motors = command['data']['motors']
-        rr.set_motors(motors['left_speed'], motors['left_direction'], motors['right_speed'], motors['right_direction'])
+        rr.set_motors(
+                motors['left_speed'], 
+                motors['left_direction'], 
+                motors['right_speed'], 
+                motors['right_direction'])
 
 asyncio.ensure_future(websockets.serve(robot, host=args.bind, port=args.port))
 asyncio.get_event_loop().run_forever()
